3a24e595e3941d9cb4b28fac40c0a80a738484b0
cdurgaprasad <dprasad.ch10@gmail.com>
2025-02-23 17:13:04 +0000

    initial commit of code

---
+++ src/components/Unlock/index.js
@@ -0,0 +1,9 @@
+// Write your code here
+import {BgContainer} from './styledComponents'
+
+const Unlock = () => {
+  const val = ''
+  return <BgContainer>HI</BgContainer>
+}
+
+export default Unlock
---
+++ src/components/Unlock/styledComponents.js
@@ -0,0 +1,6 @@
+// Style your elements here
+import styled from 'styled-components'
+
+export const BgContainer = styled.div`
+  background-image: radial-gradient(#0b0c1e, #3c2940);
+`
