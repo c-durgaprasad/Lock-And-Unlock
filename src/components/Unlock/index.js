0a03d4737d5b562b39df7f20ba7835ca9ca0a124
cdurgaprasad <dprasad.ch10@gmail.com>
2025-02-23 19:04:07 +0000

    Final code

--- src/components/Unlock/index.js
+++ src/components/Unlock/index.js
@@ -1,9 +1,30 @@
 // Write your code here
-import {BgContainer} from './styledComponents'
+import {useState} from 'react'
+import {BgContainer, LockImg, LockHeading, LockButton} from './styledComponents'
 
 const Unlock = () => {
-  const val = ''
-  return <BgContainer>HI</BgContainer>
+  const [isLocked, setLock] = useState(true)
+  const lockButtonClicked = () => {
+    setLock(prev => !prev)
+  }
+  const imageUrl = isLocked
+    ? 'https://assets.ccbp.in/frontend/hooks/lock-img.png'
+    : 'https://assets.ccbp.in/frontend/hooks/unlock-img.png'
+  const lockText = isLocked
+    ? 'Your Device is Locked'
+    : 'Your Device is Unlocked'
+  const buttonText = isLocked ? 'Unlock' : 'Lock'
+  const imgAlt = isLocked ? 'lock' : 'unlock'
+
+  return (
+    <BgContainer>
+      <LockImg src={imageUrl} alt={imgAlt} />
+      <LockHeading>{lockText}</LockHeading>
+      <LockButton type="button" onClick={lockButtonClicked}>
+        {buttonText}
+      </LockButton>
+    </BgContainer>
+  )
 }
 
 export default Unlock
--- src/components/Unlock/styledComponents.js
+++ src/components/Unlock/styledComponents.js
@@ -2,5 +2,43 @@
 import styled from 'styled-components'
 
 export const BgContainer = styled.div`
-  background-image: radial-gradient(#0b0c1e, #3c2940);
+  background-image: linear-gradient(#0b0c1e, #3c2940);
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+export const LockImg = styled.img`
+  width: 110px;
+  margin-top: 180px;
+  @media screen and (min-width: 768px) {
+    width: 80px;
+  }
+`
+
+export const LockHeading = styled.p`
+  font-family: 'Roboto';
+  color: #ffffff;
+  font-size: 15px;
+  font-weight: 400;
+  margin-top: 20px;
+  @media screen and (min-width: 768px) {
+    font-size: 20px;
+  }
+`
+
+export const LockButton = styled.button`
+  width: 98px;
+  height: 40px;
+  font-family: 'Roboto';
+  color: #ffffff;
+  border-radius: 12px;
+  background-color: #06b6d4;
+  margin-top: 200px;
+  @media screen and (min-width: 768px) {
+    width: 80px;
+    height: 40px;
+    font-size: 14px;
+    margin-top: 100px;
+  }
 `
